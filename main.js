97ab7d429b4cc2673916d41ceca114ca095955ef
nikhilmate <42518973+nikhilmate@users.noreply.github.com>
2019-05-27 16:08:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-window.onload = function () {
-  document.body.setAttribute("class", "noscroll");
+// document.body.setAttribute("class", "noscroll");
 
-  document.getElementById("overlay").style.display = "block";
-  document.getElementById("spinner").style.display = "block";
+// document.getElementById("overlay").style.display = "block";
+// document.getElementById("spinner").style.display = "block";
 
-  window.setTimeout(function () {
-    document.getElementById("spinner").style.display = "none";
-    document.getElementById("overlay").style.display = "none";
+// window.onload = function () {
+//   window.setTimeout(function () {
+//     document.getElementById("spinner").style.display = "none";
+//     document.getElementById("overlay").style.display = "none";
 
-    document.body.className = document.body.className.replace(/\bnoscroll\b/, '');
-  }, 2000);
+//     document.body.className = document.body.className.replace(/\bnoscroll\b/, '');
+//   }, 2000);
 
+// }
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+    document.body.setAttribute("class", "noscroll");
+
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("spinner").style.display = "block";
+    
+  } else if (state == 'complete') {
+      setTimeout(function(){
+        document.getElementById("spinner").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+    
+        document.body.className = document.body.className.replace(/\bnoscroll\b/, '');
+      },1000);
+  }
 }
